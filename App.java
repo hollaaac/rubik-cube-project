f04739b844920ae736a82128a1dba5cef3aa79c2
Zytaveon <35542901+Zytaveon@users.noreply.github.com>
2023-05-02 01:36:53 -0500

    Update App.java

--- App.java
+++ App.java
@@ -1,8 +1,8 @@
 package com.display;
 
-import java.io.*;
-import java.util.*;
-import javafx.application.Application;
+//import java.io.*;
+//import java.util.*;
+//import javafx.application.Application;
 
 
 public class App{
@@ -13,46 +13,11 @@ public class App{
         //Application.launch(Display_App.class, args);
         //Block block = new Block(3);
 
-        //----- Patterns -----
+        //----------------------------
+        //BiDirectional Search. WORKS!
+        //----------------------------
 
-        //White Cross       -> NWN WWW NWN NGN NGN NNN NRN NRN NNN NBN NBN NNN NON NON NNN NNN NNN NNN
-        String WhiteCross = "NWNWWWNWNNGNNGNNNNNRNNRNNNNNBNNBNNNNNONNONNNNNNNNNNNNN";
-
-        //White Face        -> WWW WWW WWW GGG NGN NNN RRR NRN NNN BBB NBN NNN OOO NON NNN NNN NNN NNN
-        String WhiteFace = "WWWWWWWWWGGGNGNNNNRRRNRNNNNBBBNBNNNNOOONONNNNNNNNNNNNN";
-
-        //Second Layer      -> WWW WWW WWW GGG GGG NNN RRR RRR NNN BBB BBB NNN OOO OOO NNN NNN NNN NNN
-        String SecondLayer = "WWWWWWWWWGGGGGGNNNRRRRRRNNNBBBBBBNNNOOOOOONNNNNNNNNNNN";
-
-        //Yellow Cross      -> WWW WWW WWW GGG GGG NNN RRR RRR NNN BBB BBB NNN OOO OOO NNN NYN YYY NYN
-        String YellowCross = "WWWWWWWWWGGGGGGNNNRRRRRRNNNBBBBBBNNNOOOOOONNNNYNYYYNYN";
-
-        //Yellow Face       -> WWW WWW WWW GGG GGG NNN RRR RRR NNN BBB BBB NNN OOO OOO NNN YYY YYY YYY
-        String YellowFace = "WWWWWWWWWGGGGGGNNNRRRRRRNNNBBBBBBNNNOOOOOONNNYYYYYYYYY";
-
-        //Yellow Corners    -> WWW WWW WWW GGG GGG GNG RRR RRR RNR BBB BBB BNB OOO OOO ONO YYY YYY YYY
-        String YellowCorners = "WWWWWWWWWGGGGGGGNGRRRRRRRNRBBBBBBBNBOOOOOOONOYYYYYYYYY";
-        
-        //BlueFace Solved   -> WWW WWW WWW GGG GGG GNG RRR RRR RNR BBB BBB BBB OOO OOO ONO YYY YYY YYY
-        String BlueFaceSolved = "WWWWWWWWWGGGGGGGNGRRRRRRRNRBBBBBBBBBOOOOOOONOYYYYYYYYY";
-
-        //GreenFace Solved  -> WWW WWW WWW GGG GGG GNG RRR RRR RNR BBB BBB BBB OOO OOO ONO YYY YYY YYY
-        String GreenFaceSolved = "WWWWWWWWWGGGGGGGGGRRRRRRRNRBBBBBBBNBOOOOOOONOYYYYYYYYY";
-
-        //RedFace Solved    -> WWW WWW WWW GGG GGG GNG RRR RRR RNR BBB BBB BBB OOO OOO ONO YYY YYY YYY
-        String RedFaceSolved = "WWWWWWWWWGGGGGGGNGRRRRRRRRRBBBBBBBNBOOOOOOONOYYYYYYYYY";
-
-        //OrangeFace Solved -> WWW WWW WWW GGG GGG GNG RRR RRR RNR BBB BBB BBB OOO OOO ONO YYY YYY YYY
-        String OrangeFaceSolved = "WWWWWWWWWGGGGGGGNGRRRRRRRNRBBBBBBBNBOOOOOOOOOYYYYYYYYY";
-
-        //Formatting in terminal. Yes it bothers me
-        System.out.println("");
-        System.out.println("-------------------------");
-        System.out.println("----------Start----------");
-        System.out.println("-------------------------");
-        System.out.println("");  
-        
-        long startTime = System.currentTimeMillis();
+        /*  long startTime = System.currentTimeMillis();
         
         Block block = new Block(3);
 
@@ -144,7 +109,33 @@ public class App{
 
         long endtime = System.currentTimeMillis();
 
-        System.out.println((endtime - startTime) / 1000 + " seconds");
+        System.out.println((endtime - startTime) / 1000 + " seconds"); */
+
+        //----------------------------
+        //BiDirectional Search. WORKS!
+        //----------------------------
+
+        //Formatting in terminal. Yes it bothers me
+        System.out.println("");
+        System.out.println("-------------------------");
+        System.out.println("----------Start----------");
+        System.out.println("-------------------------");
+        System.out.println("");  
+
+        Block block = new Block(3);
+
+        //block.randomizeCube(40);
+        block.ApplyMoves("RlFRlF");
+
+        block.printBlock();
+        System.out.println("");
+
+        //block.ApplyMoves("rDrdrdrDRDRR");
+
+        block.ApplyMoves(block.BiDiSearch());
+
+        block.printBlock();
+       
 
         //Formatting in terminal. Yes it Bothers me
         System.out.println("");
